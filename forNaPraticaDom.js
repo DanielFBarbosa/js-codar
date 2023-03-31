581c9f49e5bac7371ad0c432dd3ac4df54bddf75
Daniel Ferreira Barbosa <125709368+DanielFBarbosa@users.noreply.github.com>
2023-03-31 01:30:55 -0300

    iniciando o codigo

---
+++ forNaPraticaDom.js
@@ -0,0 +1,11 @@
+var lista = ['laranja', 'pera', 'maçã', 'limão', 'uva']
+var listaUl = document.createElement('ul')
+var body = document.getElementsByTagName('body')
+console.log(body(0))
+
+body[0].appendChild(listaUl)
+var listaNoBody = document.getElementsByTagName('ul')
+console.log(listaNoBody[0])
+
+for(var i = 0; i < lista.length; i++)
+console.log(lista[i])
